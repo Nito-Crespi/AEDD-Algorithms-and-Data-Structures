3f74e05ffea72610c3b2550b2e1193f2d4b377c1
Nito.Crespi <63932237+Nito-Crespi@users.noreply.github.com>
2020-04-25 17:00:11 -0300

    Create addition_calculator.cpp

---
+++ addition_calculator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int a, b, x;
+
+    // Input
+    cin >> a >> b;
+
+    // Processing
+    x = a + b;
+
+    // Output
+    cout << "X = " << x << endl;
+
+    return 0;
+}
